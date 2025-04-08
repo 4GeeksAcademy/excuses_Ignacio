05b658bb69ce68e19dd1edc81afbcfd7db1a140c
IgnacioBA9 <150173378+IgnacioBA9@users.noreply.github.com>
2025-04-08 09:44:42 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
+let action = ['ate ', 'peed ', 'crushed ', 'broke ', 'shitted '];
+let what = ['my homework ', 'my phone ', 'the car ', 'my car '];
+let when = ['before the class ', 'when I was sleeping ', 'while I was exercising ', 'during my lunch ', 'while I was praying '];
+let randomWhoNumber = (Math.floor(Math.random() * 4) + 1) - 1;
+let randomActionNumber = (Math.floor(Math.random() * 5) + 1) - 1;
+let randomWhatNumber = (Math.floor(Math.random() * 4) + 1) - 1;
+let randomWhenNumber = (Math.floor(Math.random() * 4) + 1) - 1;
+
 window.onload = function() {
-  //write your code here
+  document.getElementById("excuse").innerHTML = who[randomWhoNumber] + action[randomActionNumber] + what[randomWhatNumber] + when[randomWhenNumber];
   console.log("Hello Rigo from the console!");
 };
